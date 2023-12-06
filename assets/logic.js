8233ee2e38f82b91b173d2103107a073e54f18a2
Lujain <129529915+lujain142@users.noreply.github.com>
2023-12-06 19:19:15 +0300

    Add files via upload

---
+++ assets/logic.js
@@ -0,0 +1,29 @@
+// function or the pre-loader functionality
+var loader = document.querySelector('#preloader');
+
+window.addEventListener('load', function() {
+  loader.style.display = 'none'
+});
+// end of preloader function
+
+// function for changing the image on click
+function imgSlider(anything) {
+  document.querySelector('.logo1').src = anything
+};
+
+// function for changing the color of the circular background
+function changeCircleBgColor(color) {
+  // document.querySelector('.circle').style.background = color
+
+  // an alternative that uses const to avoid the long line of code
+  var circle = document.querySelector('.circle');
+  circle.style.background = color;
+};
+
+// function for displaying the menuToggle button
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
